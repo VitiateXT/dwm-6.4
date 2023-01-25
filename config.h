39ad45ff5b7e1e68e6c0648e76ea609a3def4356
VitiateXT <moritz.hoffmann@tutanota.com>
2023-01-25 13:42:28 +0100

    changed config

--- config.h
+++ config.h
@@ -24,7 +24,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", ""};
+static const char *tags[] = { "", "", "", "", ""};
 
 static const Rule rules[] = { 
 	/* xprop(1):
@@ -33,9 +33,10 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Emacs",          NULL,       NULL,       1 << 0,       0,           -1 },
-	{ "firefox",        NULL,       NULL,       1 << 1,       0,           -1 },
-	{ "mpv",      	    NULL,       NULL,       1 << 2,       0,           -1 },
-	{ "KeePassXC",	    NULL,       NULL,       1 << 3,       0,           -1 },
+	{ "st",             NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "firefox",        NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "mpv",      	    NULL,       NULL,       1 << 3,       0,           -1 },
+	{ "KeePassXC",	    NULL,       NULL,       1 << 4,       0,           -1 },
 };
 
 /* layout(s) */
